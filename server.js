3079932703f5d70ec730aa0cf55a72b860aad856
Glitch (exciting-gray-event) <none>
2023-03-30 12:27:16 +0000

    🍀🎉 Checkpoint ./server.js:54580704/1189

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+// const pug = require('pug');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
@@ -10,9 +11,11 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('views', './views/pug');
+app.set('view engine', 'pug');
 
 app.route('/').get((req, res) => {
-
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
