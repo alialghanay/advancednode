0304acfc8afbc8af160f45065f4a054417ab7349
Glitch (exciting-gray-event) <none>
2023-04-02 20:17:24 +0000

    🎳🏨 Checkpoint ./server.js:54580704/7

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ myDB(async client => {
   });
   passport.deserializeUser((id, done) => {
     myDB.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, null);
+      done(null, doc);
     });
   });
 
