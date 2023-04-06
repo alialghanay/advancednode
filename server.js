f7eb6967dc66342e9d9fc6b30750d11e8490f9d7
Glitch (exciting-gray-event) <none>
2023-04-06 14:36:55 +0000

    🚄🎷 Checkpoint ./server.js:54580704/6505

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ myDB(async client => {
     res.render('index', {
       title: 'Connected to Database',
       message: 'Please login',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
   // route end
@@ -69,12 +70,47 @@ myDB(async client => {
     }
   );
 
+  app.route('/register')
+  .post((req, res, next) => {
+    myDataBase.findOne({ username: req.body.username }, (err, user) => {
+      if (err) {
+        next(err);
+      } else if (user) {
+        res.redirect('/');
+      } else {
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: req.body.password
+        },
+          (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              // The inserted document is held within
+              // the ops property of the doc
+              next(null, doc.ops[0]);
+            }
+          }
+        )
+      }
+    })
+  },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      res.redirect('/profile');
+    }
+  );
+
   function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
       return next();
     }
     res.redirect('/');
   };
+
 
   app
  .route('/profile')
